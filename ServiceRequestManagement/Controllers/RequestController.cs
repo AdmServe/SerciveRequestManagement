01afbb60a88aa0dbd306bfcb726ae41025af0332
Mayuresh <mayuresh.warade@gmail.com>
2020-08-05 10:43:54 +0530

    Added Reuasbility

--- ServiceRequestManagement/Controllers/RequestController.cs
+++ ServiceRequestManagement/Controllers/RequestController.cs
@@ -32,29 +32,7 @@ namespace ServiceRequestManagement.Controllers
             
             foreach ( var request in allRequest){
 
-                obj.RequestId = request.Id.ToString();
-                SRMContext context = new SRMContext();
-                obj.Title = request.Title;
-
-                obj.RequestStatus = context.Status.FirstOrDefault(n => n.Id == request.StatusId).Status1;
-                obj.RequestType = context.RequestType.FirstOrDefault(n => n.Id == request.RequestTypeId).RequestType1;
-                obj.RequestDepartment = context.Department.FirstOrDefault(n => n.Id == request.DepartmentId).Name;
-                obj.RequestCategory = context.Category.FirstOrDefault(n => n.Id == request.CategoryId).Name;
-                obj.RequestSubCategory = context.Category.FirstOrDefault(n => n.Id == request.CategoryId).Name;
-
-               // obj.RequestStatus = Enum.GetName(typeof(StatusRef), request.StatusId);
-               // obj.RequestType = Enum.GetName(typeof(RequestTypeRef), request.RequestTypeId);
-               // obj.RequestDepartment = Enum.GetName(typeof(DepartmentRef), request.DepartmentId);
-               // obj.RequestCategory = Enum.GetName(typeof(CategoryRef), request.CategoryId);
-               // obj.RequestSubCategory = Enum.GetName(typeof(SubCategoryRef), request.SubCategoryId);
-
-                obj.RequestSummary = request.Summary;
-                obj.Title = request.Title;
-                obj.CreatedOn = request.CreatedOn;
-                obj.LastModifiedOn = request.LastModifiedOn;
-                obj.CreatedEmpId = request.CreatedEmpId;
-                obj.AssignedEmpId = request.AssignedEmpId;
-                obj.LastModifiedBy = request.LastModifiedBy;
+                obj.CopyData(request);
 
                 objList.Add(obj);
 
@@ -71,30 +49,9 @@ namespace ServiceRequestManagement.Controllers
         public IActionResult SingleRequest(int id)
         {
             var request = _service.GetRequestDetail(id);
-            //
-            SRMContext context = new SRMContext();
+           
             AngularRequestModel obj = new AngularRequestModel();
-
-            obj.RequestId = request.Id.ToString();
-            obj.Title = request.Title;
-
-            obj.RequestStatus = context.Status.FirstOrDefault(n => n.Id == request.StatusId).Status1;
-            obj.RequestType = context.RequestType.FirstOrDefault(n => n.Id == request.RequestTypeId).RequestType1;
-            obj.RequestDepartment = context.Department.FirstOrDefault(n => n.Id == request.DepartmentId).Name;
-            obj.RequestCategory = context.Category.FirstOrDefault(n => n.Id == request.CategoryId).Name;
-            obj.RequestSubCategory = context.Category.FirstOrDefault(n => n.Id == request.CategoryId).Name;
-            //obj.RequestStatus  = Enum.GetName( typeof(StatusRef), request.StatusId);
-            // obj.RequestType = Enum.GetName(typeof(RequestTypeRef),request.RequestTypeId);
-            //obj.RequestDepartment = Enum.GetName(typeof(DepartmentRef),request.DepartmentId);
-            // obj.RequestCategory = Enum.GetName(typeof(CategoryRef), request.CategoryId);
-            //obj.RequestSubCategory = Enum.GetName(typeof(SubCategoryRef),request.SubCategoryId);
-            obj.RequestSummary = request.Summary;
-            obj.Title = request.Title;
-            obj.CreatedOn = request.CreatedOn;
-            obj.LastModifiedOn = request.LastModifiedOn;
-            obj.CreatedEmpId = request.CreatedEmpId;
-            obj.AssignedEmpId = request.AssignedEmpId;
-            obj.LastModifiedBy = request.LastModifiedBy;
+             obj.CopyData(request);
 
             return Ok(obj);
         }
@@ -103,33 +60,9 @@ namespace ServiceRequestManagement.Controllers
         [HttpPut("UpdateRequest/{id}")]
         public IActionResult UpdateRequest(int id, [FromBody] AngularRequestModel obj)
         {
-            SRMContext context = new SRMContext();
-            Request request = new Request();
-
-            request.Id= Int32.Parse(obj.RequestId);
-            request.Title=obj.Title;
-
-
-            request.StatusId = context.Status.FirstOrDefault(n => n.Status1 == obj.RequestStatus).Id;
-            request.RequestTypeId = context.RequestType.FirstOrDefault(n => n.RequestType1 == obj.RequestType).Id;
-            request.DepartmentId = context.Department.FirstOrDefault(n => n.Name == obj.RequestDepartment).Id;
-            request.CategoryId = context.Category.FirstOrDefault(n => n.Name == obj.RequestCategory).Id;
-            request.SubCategoryId = context.Category.FirstOrDefault(n => n.Name == obj.RequestSubCategory).Id;
-            //  request.StatusId = (int)Enum.Parse(typeof(StatusRef),obj.RequestStatus);
-            // request.RequestTypeId = (int)Enum.Parse(typeof(RequestTypeRef), obj.RequestType);
-            // request.DepartmentId = (int)Enum.Parse(typeof(DepartmentRef), obj.RequestDepartment);
-            //request.CategoryId = (int)Enum.Parse(typeof(CategoryRef), obj.RequestCategory);
-            //request.SubCategoryId = (int)Enum.Parse(typeof(SubCategoryRef), obj.RequestSubCategory);
-
-            request.Summary=obj.RequestSummary ;
-            request.CreatedOn=obj.CreatedOn ;
-            request.LastModifiedOn=obj.LastModifiedOn ;
-            request.CreatedEmpId=obj.CreatedEmpId;
-            request.AssignedEmpId= obj.AssignedEmpId ;
-            request.LastModifiedBy = obj.LastModifiedBy;
-            
-
-            _service.UpdateRequest(id, request);
+           
+           
+            _service.UpdateRequest(id, obj.SendData());
 
             return Ok(obj);
 
--- ServiceRequestManagement/RequestFormatter/AngularRequestModel.cs
+++ ServiceRequestManagement/RequestFormatter/AngularRequestModel.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ServiceRequestManagement.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace ServiceRequestManagement.RequestFormatter
@@ -60,5 +63,65 @@ namespace ServiceRequestManagement.RequestFormatter
 
         public string LastModifiedBy { get; set; }
 
+
+        public  void CopyData(Request request)
+        {
+            SRMContext context = new SRMContext();
+
+
+            this.RequestId = request.Id.ToString();
+            this.Title = request.Title;
+
+            this.RequestStatus = context.Status.FirstOrDefault(n => n.Id == request.StatusId).Status1;
+            this.RequestType = context.RequestType.FirstOrDefault(n => n.Id == request.RequestTypeId).RequestType1;
+            this.RequestDepartment = context.Department.FirstOrDefault(n => n.Id == request.DepartmentId).Name;
+            this.RequestCategory = context.Category.FirstOrDefault(n => n.Id == request.CategoryId).Name;
+            this.RequestSubCategory = context.Category.FirstOrDefault(n => n.Id == request.CategoryId).Name;
+            //this.RequestStatus  = Enum.GetName( typeof(StatusRef), request.StatusId);
+            // this.RequestType = Enum.GetName(typeof(RequestTypeRef),request.RequestTypeId);
+            //this.RequestDepartment = Enum.GetName(typeof(DepartmentRef),request.DepartmentId);
+            // this.RequestCategory = Enum.GetName(typeof(CategoryRef), request.CategoryId);
+            //this.RequestSubCategory = Enum.GetName(typeof(SubCategoryRef),request.SubCategoryId);
+
+            this.RequestSummary = request.Summary;
+            this.Title = request.Title;
+            this.CreatedOn = request.CreatedOn;
+            this.LastModifiedOn = request.LastModifiedOn;
+            this.CreatedEmpId = request.CreatedEmpId;
+            this.AssignedEmpId = request.AssignedEmpId;
+            this.LastModifiedBy = request.LastModifiedBy;
+
+        }
+
+        public Request SendData()
+        {
+            Request request = new Request();
+            SRMContext context = new SRMContext();
+
+            request.Id = Int32.Parse(this.RequestId);
+            request.Title = this.Title;
+
+
+            request.StatusId = context.Status.FirstOrDefault(n => n.Status1 == this.RequestStatus).Id;
+            request.RequestTypeId = context.RequestType.FirstOrDefault(n => n.RequestType1 == this.RequestType).Id;
+            request.DepartmentId = context.Department.FirstOrDefault(n => n.Name == this.RequestDepartment).Id;
+            request.CategoryId = context.Category.FirstOrDefault(n => n.Name == this.RequestCategory).Id;
+            request.SubCategoryId = context.Category.FirstOrDefault(n => n.Name == this.RequestSubCategory).Id;
+            //  request.StatusId = (int)Enum.Parse(typeof(StatusRef),obj.RequestStatus);
+            // request.RequestTypeId = (int)Enum.Parse(typeof(RequestTypeRef), obj.RequestType);
+            // request.DepartmentId = (int)Enum.Parse(typeof(DepartmentRef), obj.RequestDepartment);
+            //request.CategoryId = (int)Enum.Parse(typeof(CategoryRef), obj.RequestCategory);
+            //request.SubCategoryId = (int)Enum.Parse(typeof(SubCategoryRef), obj.RequestSubCategory);
+
+            request.Summary = this.RequestSummary;
+            request.CreatedOn = this.CreatedOn;
+            request.LastModifiedOn = this.LastModifiedOn;
+            request.CreatedEmpId = this.CreatedEmpId;
+            request.AssignedEmpId = this.AssignedEmpId;
+            request.LastModifiedBy = this.LastModifiedBy;
+            return request;
+        }
     }
+
+  
 }
